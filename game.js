86b48d2a8f42b397fa0671eabaccca5024cf2193
Eric Honas <honased@gmail.com>
2022-11-22 14:32:34 -0600

    Game complete.

--- game.js
+++ game.js
@@ -24,6 +24,10 @@ function help()
 currentRoom = null;
 player = null;
 gagVisited = false;
+checkingKeypad = false;
+checkingChalk = false;
+determinationUsed = false;
+gameOver = false;
 
 class Item
 {
@@ -31,6 +35,7 @@ class Item
     {
         this.name = name;
         this.pickable = pickable;
+        this.use = null;
     }
 }
 
@@ -50,6 +55,17 @@ class Player
             outputText(" " + this.inventory[i].name);
         }
         outputText("---------");
+    }
+
+    removeItem(item)
+    {
+        for(var i = 0; i < this.inventory.length; i++)
+        {
+            if(this.inventory[i].name.toLowerCase() == item.toLowerCase())
+            {
+                this.inventory.splice(i, 1);
+            }
+        }
     }
 }
 
@@ -155,12 +171,36 @@ class GagArea extends ParRoom
     }
 }
 
+class Tablet extends ParRoom
+{
+    constructor()
+    {
+        super();
+    }
+
+    getDescription()
+    {
+        return "As you reach the summit of the hill, you notice a stone tablet lying on the ground. You can barely make out the markings of a name on the stone: \"Peter.\" Inspiration seems ready to strike. It is time to prove that you are an artist...";
+    }
+}
+
 class ForestPath1 extends ParRoom
 {
     constructor()
     {
         super();
         this.chalkItem = new Item("Chalk", true);
+        this.chalkItem.use = (function() {
+            if( currentRoom instanceof Tablet )
+            {
+                outputText("What would you like to draw on the stone tablet:");
+                checkingChalk = true;
+            }
+            else
+            {
+                outputText("Now doesn't seem like the best time to use chalk.");
+            }
+        });
         this.items.push(this.chalkItem);
         this.north = new Clearing();
         this.north.south = this;
@@ -184,6 +224,8 @@ class Clearing extends ParRoom
         this.east.west = this;
         this.north = new Zelinsky();
         this.north.south = this;
+        this.west = new Flower();
+        this.west.east = this;
     }
 
     getDescription()
@@ -272,11 +314,17 @@ class Clearing2 extends ParRoom
     constructor()
     {
         super();
+        this.poem = null;
     }
 
     getDescription()
     {
-        return "You arrive in a clearing where you see a child and a dog eating pancakes. This somehow feels peaceful, yet you are not sure if this is a hallucination. You notice a path that leads back east.";
+        var retTex = "You arrive in a clearing where you see a child and a dog eating pancakes. This somehow feels peaceful, yet you are not sure if this is a hallucination. "
+
+        if( this.poem != null && this.items.includes(this.poem) ) retTex += "You also see a poem resting on the floor waiting to be carried away by the wind. ";
+
+        retTex += "You notice a path that leads back east.";
+        return retTex;
     }
 }
 
@@ -286,6 +334,10 @@ class Zelinsky extends ParRoom
     {
         super();
         this.keypad = new Item("Keypad", false);
+        this.keypad.use = (function() {
+            outputText("Enter the combination:");
+            checkingKeypad = true;
+        });
         this.items.push(this.keypad);
     }
 
@@ -294,6 +346,115 @@ class Zelinsky extends ParRoom
         var retText = "You see an old building just north of you with the text \"Zelinsky's\" adorning the space above the door."
         if( this.items.includes(this.keypad) ) retText += " The door appears to be locked. You notice a keypad near the door that might unlock it.";
         retText += " Otherwise, you see a path leading back south to a clearing in the forest.";
+        return retText;
+    }
+}
+
+class Gymnasium extends ParRoom
+{
+    constructor()
+    {
+        super();
+    }
+
+    getDescription()
+    {
+        return "You stand in the center of a gymnasium with a spotlight shining brightly on you. In the bleachers, cardboard cutouts of different people stand up expectantly waiting for something. Perhaps they want something from you... A door to the south leads out of the gym.";
+    }
+}
+
+class Flower extends ParRoom
+{
+    constructor()
+    {
+        super();
+        this.flower = new Item("Flower", false);
+        this.items.push(this.flower);
+        this.west = new Hill();
+        this.west.east = this;
+    }
+
+    getDescription()
+    {
+        var retText = "You come across a patch of dirt"
+        if( this.items.includes(this.flower) ) retText += " with a single golden flower sprouting out of it. Something about this flower seems to draw you in. Perhaps it LOVEs you...";
+        else retText += " with a single stone bearing the name \"Asriel.\""
+        retText += " You notice paths that lead both east and west."
+        return retText;
+    }
+}
+
+class Hill extends ParRoom
+{
+    constructor()
+    {
+        super();
+        this.bubbles = new Item("Bubbles", false);
+        this.bubbles.use = (function() {
+            if( currentRoom instanceof Clearing2 )
+            {
+                player.removeItem("bubbles");
+                outputText("You begin to blow bubbles which causes the child to erupt into contagious laughter. As the child laughs, a piece of paper with a poem written on it falls out of his pockets and onto the ground.");
+                currentRoom.poem = new Item("Poem", true);
+                currentRoom.poem.use = (function() {
+                    if( currentRoom instanceof Gymnasium )
+                    {
+                        player.removeItem("poem");
+                        outputText("You read the poem out loud and you swear you can hear applause. Suddenly, a button falls from the ceiling. Upon closer inspection, you see the word \"SAVE\" written on the top of the button. You pick it up and decide to hold on to it for now.");
+                        var saveItem = new Item("SAVE", true);
+                        saveItem.use = (function() {
+                            if( currentRoom instanceof Flower )
+                            {
+                                outputText("As you push the button, the flower disappears and in it's place a stone appears. Looking at the stone, you see the name \"Asriel\" engraved in it. As you look away, you suddenly feel your pockets become heavier, but with what?");
+                                player.removeItem("save");
+                                currentRoom.items = [];
+                                var determinationItem = new Item("DETERMINATION", true);
+                                determinationItem.use = (function() {
+                                    if( currentRoom instanceof Hill )
+                                    {
+                                        outputText("You are filled with determination. The hill no longer seems as daunting...");
+                                        determinationUsed = true;
+                                        currentRoom.north = new Tablet();
+                                        currentRoom.north.south = currentRoom;
+                                        player.removeItem("determination");
+                                    }
+                                    else
+                                    {
+                                        outputText("You don't seem to need determination right now...");
+                                    }
+                                });
+                                player.inventory.push(determinationItem);
+                            }
+                            else
+                            {
+                                outputText("You pushed the button, but it did nothing... What did you think it would do, save the game?");
+                            }
+                        });
+                        player.inventory.push(saveItem);
+                    }
+                    else
+                    {
+                        outputText("You read the poetry, but it seemed to do nothing. Let's put that poem away for later...");
+                    }
+                });
+                currentRoom.items.push(currentRoom.poem);
+            }
+            else
+            {
+                outputText("Bubbles surely are fun to play with, but this doesn't seem the time nor place to use them.");
+            }
+        });
+        this.items.push(this.bubbles);
+    }
+
+    getDescription()
+    {
+        var retText = "You walk into into an grassy field surrounded with trees to the west and south. A path leads back to the east, "
+
+        if( determinationUsed ) retText += "and a steep hill to the north seems easy to climb to you.";
+        else retText += "and a steep hill to the north seems insurmountable to you."
+
+        if( this.items.includes(this.bubbles) ) retText += " You also notice a bottle of bubbles laying on the ground.";
         return retText;
     }
 }
@@ -314,6 +475,47 @@ function grab(item)
         }
     }
     outputText("There doesn't appear to be " + item + ".");
+}
+
+function use(item)
+{
+    if(item != null && item.length > 0)
+    {
+        // Check room
+        for(var i = 0; i < currentRoom.items.length; i++)
+        {
+            if(currentRoom.items[i].name.toLowerCase() == item)
+            {
+                if(currentRoom.items[i].use != null)
+                {
+                    currentRoom.items[i].use();
+                }
+                else
+                {
+                    outputText("Can't use that item right now.");
+                }
+                return;
+            }
+        }
+
+        // Check inventory
+        for(var i = 0; i < player.inventory.length; i++)
+        {
+            if(player.inventory[i].name.toLowerCase() == item)
+            {
+                if(player.inventory[i].use != null)
+                {
+                    player.inventory[i].use();
+                }
+                else
+                {
+                    outputText("Can't use that item right now.");
+                }
+                return;
+            }
+        }
+    }
+    outputText("There is no item named '" + item + "' to use.");
 }
 
 function goDirection(direction)
@@ -390,6 +592,11 @@ function checkGeneralCommands(text)
                 grab(cmds[1]);
                 return true;
             }
+            if( cmds[0] == "use" )
+            {
+                use(cmds[1]);
+                return true;
+            }
             break;
     }
 
@@ -399,7 +606,7 @@ function checkGeneralCommands(text)
 currentRoom = new StartingArea();
 player = new Player();
 
-outputText("You are an arist. At least, you think you are. Nothing you've done ever seems to get much attention, and you doubt your abilities. Needing a refresh, you decided to venture out into the woods to reinvigorate your mind and soul. However, you have gotten lost and can't seem to find your way out. While it is day now, it will get dark soon, and you have heard rumors about the forest at night...\n\nTHE ADVENTURE BEGINS\n");
+outputText("You are an arist. At least, you think you are. Nothing you've done ever seems to get much attention, and you doubt your abilities. Needing to refresh, you decided to venture out into the woods to reinvigorate your mind and soul. However, you have gotten lost and can't seem to find your way out. While it is day now, it will get dark soon, and you have heard rumors about the forest at night...\n\nTHE ADVENTURE BEGINS\n");
 
 outputText(currentRoom.getDescription());
 
@@ -411,7 +618,42 @@ function handleInput()
     document.getElementById("command").value = "";
     outputText(text);
     var checkText = text.toLowerCase();
-    if(!currentRoom.checkCommand(checkText) &&
+    if(gameOver)
+    {
+        outputText("The game's over... There is no more story... Go spend time with your friends and family...");
+    }
+    else if(checkingKeypad)
+    {
+        if(checkText.trim() == "0498")
+        {
+            currentRoom.items = [];
+            currentRoom.north = new Gymnasium();
+            currentRoom.north.south = currentRoom;
+            outputText("The keypad emits one beep, and the door slowly opens revealing a passage inside to the north.");
+        }
+        else
+        {
+            outputText("The keypad beeps twice signalling that the code is incorrect..")
+        }
+        checkingKeypad = false;
+    }
+    else if(checkingChalk)
+    {
+        if(checkText.trim().toLowerCase() == "horse")
+        {
+            player.removeItem("chalk");
+            outputText("As you draw the horse, it fills you with joy and inspiration. You realize you are an artist, and you always have been. If only you could remember how to leave this forest...");
+            outputText("\nTHE END\n");
+            outputText("Congratulations! You have successfully completed \"Creative Project\" and are hereby declared a level-5 Adventurer.");
+            gameOver = true;
+        }
+        else
+        {
+            outputText("You try drawing \"" + checkText + "\" with the chalk on the stone tablet, but it doesn't fill you with inspiration. You quickly wipe it away.");
+        }
+        checkingChalk = false;
+    }
+    else if(!currentRoom.checkCommand(checkText) &&
        !checkGeneralCommands(checkText))
     {
         outputText("I don't know what '" + text + "' means.");
